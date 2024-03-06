dd5fb58733d1d3020ba392c90e2a071e87ab82d2
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2024-03-06 16:31:11 -0300

    Pynfedance: Python converter for Brazilian Electronic Invoice

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="pynfedanfe",
+    version="0.0.1",
+    description="Python converter for Brazilian Electronic Invoice",
+    long_description="""
+    Python converter for Brazilian Electronic Invoice (NFe)
+    XMLs to Brazilian Auxiliary Electronic Invoice Document (DANFE)
+    """,
+    url="https://github.com/Engenere/pynfedanfe",
+    author="Cristiano Mafra Junior",
+    license="AGPL-3.0",
+)
---
+++ test/test_pynfedanfe.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import unittest
+from xml.etree import ElementTree as ET
+from io import BytesIO
+from os import path
+
+sys.path.append(path.join(path.dirname(__file__), ".."))
+from pynfedanfe import pynfedanfe
+
+
+class TestPynfedance(unittest.TestCase):
+    def setUp(self):
+        test_data_path = os.path.join(
+            os.path.dirname(__file__), "data", "NFe_teste_cee.xml"
+        )
+        with open(test_data_path, "r") as f:
+            self.cce_xml = f.read()
+        self.xml_root = ET.fromstring(self.cce_xml)
+
+    def test_format_cnpj_cpf(self):
+        nfedanfe_instance = pynfedanfe
+        cpf = nfedanfe_instance.format_cnpj_cpf("76586507812")
+        self.assertEqual("765.865.078-12", cpf)
+
+    def test_format_number(self):
+        nfedanfe_instance = pynfedanfe
+        number = nfedanfe_instance.format_number("19500")
+        self.assertEqual("19.500,00", number)
+
+    def test_tagtext_found(self):
+        nfedanfe_instance = pynfedanfe
+        text = nfedanfe_instance.tagtext(self.xml_root, "cTag")
+        self.assertEqual(text, "")
+
+    def test_create_danfe_pdf(self):
+        self.xml_root = ET.fromstring(self.cce_xml)
+
+        object_danfe = pynfedanfe.danfe(
+            list_xml=[self.xml_root],
+        )
+
+        tmpDanfe = BytesIO()
+        object_danfe.writeto_pdf(tmpDanfe)
+        danfe_pdf = tmpDanfe.getvalue()
+        tmpDanfe.close()
+        self.assertTrue(len(danfe_pdf) > 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
