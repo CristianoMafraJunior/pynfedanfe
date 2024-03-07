5bda4690c5f70cd63eba89f0cb1a25556dbc073a
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2024-03-07 10:57:45 -0300

    Pynfedance: Fix import

--- test/test_pynfedanfe.py
+++ test/test_pynfedanfe.py
@@ -1,12 +1,9 @@
 import os
-import sys
 import unittest
 from xml.etree import ElementTree as ET
 from io import BytesIO
-from os import path
 
-sys.path.append(path.join(path.dirname(__file__), ".."))
-from pynfedanfe import pynfedanfe
+from pynfedanfe import danfe
 
 
 class TestPynfedance(unittest.TestCase):
@@ -19,24 +16,24 @@ class TestPynfedance(unittest.TestCase):
         self.xml_root = ET.fromstring(self.cce_xml)
 
     def test_format_cnpj_cpf(self):
-        nfedanfe_instance = pynfedanfe
+        nfedanfe_instance = danfe
         cpf = nfedanfe_instance.format_cnpj_cpf("76586507812")
         self.assertEqual("765.865.078-12", cpf)
 
     def test_format_number(self):
-        nfedanfe_instance = pynfedanfe
+        nfedanfe_instance = danfe
         number = nfedanfe_instance.format_number("19500")
         self.assertEqual("19.500,00", number)
 
     def test_tagtext_found(self):
-        nfedanfe_instance = pynfedanfe
+        nfedanfe_instance = danfe
         text = nfedanfe_instance.tagtext(self.xml_root, "cTag")
         self.assertEqual(text, "")
 
     def test_create_danfe_pdf(self):
         self.xml_root = ET.fromstring(self.cce_xml)
 
-        object_danfe = pynfedanfe.danfe(
+        object_danfe = danfe.danfe(
             list_xml=[self.xml_root],
         )
 
